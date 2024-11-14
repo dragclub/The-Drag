529a181b8880c97ffc6895ef207a44c0d74ff079
dragclub <bodhbusiness@gmail.com>
2024-11-14 23:29:58 +0530

    Delete The-drag-FE directory

--- The-drag-FE/src/assets/creators.js
+++
@@ -1,3 +0,0 @@
-import youthiyapa from './youthiyapa.png'
-const creators=[{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa},{name:"youthiyapa",image:youthiyapa}];
-export default creators;
--- The-drag-FE/src/assets/images.js
+++
@@ -1,7 +0,0 @@
-import  linkedin from './linkedin.svg'
-import insta from './insta.svg'
-import twitter from './twitter.svg'
-import youtube from './youtube.svg'
-import facebook from './facebook.svg'
-
-export default {linkedin,insta,twitter,youtube,facebook};
